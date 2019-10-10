dfccf4f008b63200e842d8bf072fe3b2bf67338e
katerhoades <katerhoades@gmail.com>
2019-10-09 18:15:10 -0700

    done

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
+var img1;
+
+function preload() {
+img1 = loadImage("images/computer3.png")
+}
 
 function setup() {
-  createCanvas(480, 120);
+  createCanvas(2000, 1100);
 }
 
 function draw() {
-  background(204);
-  strokeWeight(10);
-  arc(90, 60, 80, 80, QUARTER_PI, PI + QUARTER_PI);
-  strokeWeight(20);
-  ellipse(20, 20, 120, 120)
-
+  background(0, 0, 0);
+  imageMode(CENTER);
+  image(img1, mouseX, mouseY);
 }
