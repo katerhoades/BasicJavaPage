fecf4c2a1e57fc5273567d65f7517a05ed95d6bd
Kate Rhoades <katerhoades@gmail.com>
2019-10-09 18:59:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 var img;
 function preload() {
-  img = loadImage("/images/Scribble.png");
+  img = loadImage("Scribble.png"); 
 }
 
-function setup () {
+function setup () {  
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(0,100,10,30);
+  background(100,0,10);
   rect (10,10,200,10)
   fill (3,85,3)
   image(img,mouseX,mouseY)
